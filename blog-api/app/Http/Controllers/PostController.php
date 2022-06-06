7b3aab6b3ff58e6cd442568fc706d336d46984a9
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-06-05 22:37:34 -0300

    Post controller, resource and factory

---
+++ blog-api/app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return PostResource::collection(Post::latest()->wherePublished(true)->get());
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
+}

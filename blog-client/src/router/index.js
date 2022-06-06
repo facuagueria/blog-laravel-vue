ec44bdd331159abf26f54b6c5b076dda68ff737c
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-06-05 22:36:48 -0300

    Create vuex store and vue router

--- blog-client/src/router/index.js
+++ blog-client/src/router/index.js
@@ -1,15 +1,41 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 import HomeView from "../views/HomeView.vue";
+import PostView from "../views/PostView.vue";
+import LoginView from "../views/LoginView.vue";
+import PostsView from "../views/admin/PostsView.vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/admin/login",
+    name: "admin.login",
+    component: LoginView,
+  },
+  {
+    path: "/admin/posts",
+    name: "admin.posts",
+    component: PostsView,
+    beforeEnter: (to, from, next) => {
+      if (!store.getters.authenticated) {
+        return next({ name: "admin.login" });
+      }
+      return next();
     },
-  ],
-});
+  },
+  {
+    path: "/posts/:slug",
+    name: "post",
+    component: PostView,
+    props: true,
+  },
+];
 
-export default router;
+export default createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
---
+++ blog-client/src/store/index.js
@@ -0,0 +1,46 @@
+import { createStore } from "vuex";
+import axios from "axios";
+
+export default createStore({
+  state: {
+    authenticated: false,
+    user: null,
+  },
+  getters: {
+    authenticated(state) {
+      return state.authenticated;
+    },
+    user(state) {
+      return state.user;
+    },
+  },
+  mutations: {
+    SET_AUTHENTICATED(state, authenticated) {
+      state.authenticated = authenticated;
+    },
+    SET_USER(state, user) {
+      state.user = user;
+    },
+  },
+  actions: {
+    authenticate({ commit }) {
+      return axios
+        .get("/api/user")
+        .then((response) => {
+          commit("SET_AUTHENTICATED", true);
+          commit("SET_USER", response.data);
+        })
+        .catch(() => {
+          commit("SET_AUTHENTICATED", false);
+          commit("SET_USER", null);
+        });
+    },
+    async login({ dispatch }, credentials) {
+      //csrf
+      await axios.get("/sanctum/csrf-cookie");
+      await axios.post("/login", credentials);
+
+      dispatch("authenticate");
+    },
+  },
+});

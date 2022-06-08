1b4fb75a3f2db9c01a7583ff37ab782c7a7d969c
facundoagueria1@gmail.com <facundoagueria1@gmail.com>
2022-06-07 22:27:05 -0300

    Show, store and update AdminPost

--- blog-api/app/Http/Controllers/Admin/AdminPostController.php
+++ blog-api/app/Http/Controllers/Admin/AdminPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostEditResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -17,5 +18,31 @@ class AdminPostController extends Controller
     public function index()
     {
         return PostResource::collection(Post::latest()->get());
+    }
+
+    public function show(Post $post)
+    {
+        return new PostEditResource($post);
+    }
+
+    public function store()
+    {
+        $post = Post::create([
+            'title' => 'Untitled Post'
+        ]);
+
+        return new PostResource($post);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'teaser' => 'nullable',
+            'body' => 'nullable',
+            'published' => 'boolean'
+        ]);
+
+        $post->update($request->only('title', 'title', 'teaser', 'body', 'published'));
     }
 }
